0b22e8028d4a5654f90d32d11a8f671a79425438
Md Mustak <mustakarman560@gmail.com>
2025-07-11 11:03:30 +0530

    js

--- 11th july/Pro7.js
+++ 11th july/Pro7.js
@@ -2,5 +2,7 @@ function check(value, index){
     return value%2 === 0;
 }
 var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var arr2 = arr1.filter(check);
+// var arr2 = arr1.filter(check);
+var arr2 = arr1.filter(num=>num<6);
+
 console.log(arr2);
---
+++ 11th july/pro8.js
@@ -0,0 +1,7 @@
+var arr1 = [45, 67, 89, 90, 97, 100];
+
+arr1.map(num => {
+    return num * 2;
+}).forEach(num => {
+    console.log(num);
+});
